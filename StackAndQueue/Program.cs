2cce3422334d68c5719abb352a10f89b4ae0e3ae
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-09 19:48:46 +0530

    [Arjun] Add ; Added Switched

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,14 +5,20 @@ class program
 {
     static void Main(string[] args)
     {
-        LinkListStack linkedListStack = new LinkListStack();
-        linkedListStack.Push(70);
-        linkedListStack.Push(30);
-        linkedListStack.Push(56);
-        linkedListStack.Display();
-        linkedListStack.peek();
-        linkedListStack.pop();
-        linkedListStack.isEmpty();
-        linkedListStack.Display();
+       // LinkListStack linkedListStack = new LinkListStack();
+        //linkedListStack.Push(70);
+        //linkedListStack.Push(30);
+        //linkedListStack.Push(56);
+        //linkedListStack.Display();
+        //linkedListStack.peek();
+        //linkedListStack.pop();
+        //linkedListStack.isEmpty();
+        //linkedListStack.Display();
+
+        LinkListQueue linkedListQueue = new LinkListQueue();
+        linkedListQueue.Enqueue(56);
+        linkedListQueue.Enqueue(30);
+        linkedListQueue.Enqueue(70);
+        linkedListQueue.Display();
     }
 }
