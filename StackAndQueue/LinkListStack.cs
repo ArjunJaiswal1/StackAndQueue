9528087b684b46b3ebbf97ab2422ef6eb16f8136
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-09 19:39:49 +0530

    [Arjun] Add ; Added Switched

---
+++ StackAndQueue/LinkListStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class LinkListStack
+    {
+        private Node top;
+        public LinkListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int Value)
+        {
+            Node node = new Node(Value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", Value);
+
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StackAndQueue;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        LinkListStack linkedListStack = new LinkListStack();
+        linkedListStack.Push(70);
+        linkedListStack.Push(30);
+        linkedListStack.Push(56);
+        linkedListStack.Display();
+    }
+}
